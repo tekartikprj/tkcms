d7950c1d2f9be6f26c8552577cbf212f780749e5
Alexandre Roux <alex@tekartik.com>
2025-01-07 15:31:05 +0100

    fix lints

--- flutter_packages/tkcms_admin_app/lib/sembast/local_db_bloc.dart
+++ flutter_packages/tkcms_admin_app/lib/sembast/local_db_bloc.dart
@@ -2,6 +2,7 @@ import 'package:tkcms_admin_app/sembast/sembast.dart';
 
 class LocalDbBloc {
   var ready = () async {
+    // ignore: unused_local_variable
     var db = globalSembastDatabasesContext.factory.openDatabase('local.db');
   }();
 }
