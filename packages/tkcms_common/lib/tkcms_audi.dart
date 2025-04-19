2fbd29b988476869f85a24021c86f2a39177e3a6
Alexandre Roux <alex@tekartik.com>
2025-04-19 18:15:01 +0200

    feat: export helpers

--- packages/tkcms_common/lib/tkcms_audi.dart
+++ packages/tkcms_common/lib/tkcms_audi.dart
@@ -1,2 +1,3 @@
+export 'package:tekartik_app_rx/helpers.dart';
 export 'package:tekartik_app_rx_bloc/auto_dispose_base_bloc.dart';
 export 'package:tekartik_app_rx_bloc/auto_dispose_state_base_bloc.dart';
