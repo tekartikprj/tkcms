27ac5c3280dcf229f6c3cbe71db43ab85b54953e
Alexandre Roux <alex@tekartik.com>
2026-07-01 10:54:46 +0200

    feat: add emulator support for Firebase services context

--- packages/tkcms_common/lib/src/firebase/firebase_rest.dart
+++ packages/tkcms_common/lib/src/firebase/firebase_rest.dart
@@ -4,6 +4,8 @@ import 'package:tekartik_firebase_functions_call_rest/functions_call_rest.dart';
 import 'package:tekartik_firebase_rest/firebase_rest.dart';
 import 'package:tkcms_common/tkcms_firebase.dart';
 
+import '../../tkcms_firestore.dart';
+
 /// Init rest services.
 Future<FirebaseServicesContext> initFirebaseServicesRest({
   FirebaseAppOptions? appOptions,
@@ -22,4 +24,13 @@ Future<FirebaseServicesContext> initFirebaseServicesRest({
     functionsCallRegion: regionBelgium,
   );
   return firebaseServicesContext;
+}
+
+/// Use emulator
+extension FirebaseContextRestExt on FirebaseContext {
+  /// Use emulator.
+  Future<void> useEmulator() async {
+    await (auth as FirebaseAuthRest).useAuthEmulator('localhost', 9099);
+    await (firestore as FirestoreRest).useFirestoreEmulator('localhost', 8080);
+  }
 }
